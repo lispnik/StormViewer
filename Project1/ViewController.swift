593b6edd3e4d8b373112870bc1acfd3b6531c34f
Matthew Kennedy <burnsidemk@gmail.com>
2020-04-07 23:10:24 -0500

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Matthew Kennedy on 4/7/20.
+//  Copyright © 2020 Matthew Kennedy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
